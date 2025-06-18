0bb5566ad368b13fc2cb8cf2ea7116d452cc1313
Mike Slinn <mslinn@mslinn.com>
2025-06-18 14:59:00 -0400

    -

--- lib/nugem/nugem_module.rb
+++ lib/nugem/nugem_module.rb
@@ -51,6 +51,27 @@ module Nugem
     spec&.full_gem_path
   end
 
+  def self.positional_parameters
+    ARGV.reject { |x| x.start_with? '-' }
+  end
+
+  def self.run_me
+    pp = ::Nugem.positional_parameters
+    ::Nugem.help if pp.empty?
+    gem_type = pp.first
+    case gem_type
+    when 'gem'
+      nugem = Options.new
+      nugem.parse_options
+    when 'jekyll'
+      nugem = JekyllOptions.new
+      nugem.parse_options
+    else
+      puts "Error: unrecognized gem type '#{gem_type}'."
+      exit 2
+    end
+  end
+
   def self.template_directory
     File.join gem_path(__FILE__), 'templates'
   end
