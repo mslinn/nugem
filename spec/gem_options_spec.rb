81d55afe6c457802f65194ca278d4f900d9fd19f
Mike Slinn <mslinn@mslinn.com>
2025-06-21 12:12:39 -0400

    -

--- spec/gem_options_spec.rb
+++ spec/gem_options_spec.rb
@@ -10,22 +10,22 @@ class GemOptionsTest
   ARGV.clear
   ARGV = '-L debug gem'.freeze
 
-  DEFAULT_TEST_DIR = File.join(Dir.home, 'nugem_generated').freeze
-  TEST_DIR = File.join(Dir.tmpdir, 'nugem/test').freeze
+  DEFAULT_OUT_DIR = File.join(Dir.home,   'nugem_generated').freeze
+  TEST_OUT_DIR    = File.join(Dir.tmpdir, 'nugem_test').freeze
 
   RSpec.describe ::Nugem::Options do
     after(:context) do # rubocop:disable RSpec/BeforeAfterAll
-      FileUtils.rm_rf TEST_DIR, secure: true
+      FileUtils.rm_rf TEST_OUT_DIR, secure: true
     end
 
     before(:context) do # rubocop:disable RSpec/BeforeAfterAll
-      FileUtils.rm_rf TEST_DIR, secure: true
+      FileUtils.rm_rf TEST_OUT_DIR, secure: true
     end
 
     it 'tests default values' do
       gem_options = described_class.new
       options = gem_options.parse_options(parse_dry_run: true)
-      expect(options[:out_dir]).to eq(DEFAULT_TEST_DIR)
+      expect(options[:out_dir]).to eq(DEFAULT_OUT_DIR)
     end
   end
 end
