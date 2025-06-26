c332ba9052580350a65177577dbe698cb820bf1f
Mike Slinn <mslinn@mslinn.com>
2025-06-26 07:15:56 -0400

    -

--- lib/nested_options_parser.rb
+++ lib/nested_options_parser.rb
@@ -1,7 +1,43 @@
+require 'optparse'
+
 class NestedOptionParser
-  def initialize(option_parser_proc, subcommand_parser_procs = [])
-    @option_parser = OptionParser.new option_parser_proc
+  def initialize(default_options, option_parser_proc, subcommand_parser_procs = [], argv: ARGV)
+    @unmatched_args = []
+    # @option_parser = evaluate option_parser_proc
     @subcommand_parser_procs = subcommand_parser_procs
-    yield self if block_given?
+    evaluate(default_options, argv, &option_parser_proc)
+    puts "ARGV=#{ARGV}"
+    puts "@unmatched_args=#{@unmatched_args}"
+  end
+
+  # How to add this to every option_parser_proc:
+  # parser.on /.*/ { |subcommand| subcommand.call } # Somehow the modified ARGV from the subcommand
+  # needs to affect the caller's ARGV
+
+  def evaluate(default_options, argv, &op_proc)
+    options = default_options
+    OptionParser.new do |parser|
+      parser.default_argv = argv
+      parser.raise_unknown = false
+      yield parser, op_proc
+      parser.on(/.*/) do |arg|
+        puts "Unmatched: #{arg}"
+        @unmatched_args << arg
+      end
+    end.order! into: options
+    puts "After parsing, options=#{options}"
+    options
+  rescue OptionParser::InvalidOption => e
+    puts e.message
+    exit 1
   end
 end
+
+
+my_option_parser_proc = proc do |parser|
+  puts "Evaluating my_option_parser_proc"
+  parser.on '-h', '--help'
+  parser.on '-o', '--out_dir OUT_DIR'
+end
+
+NestedOptionParser.new({}, my_option_parser_proc, argv: %w[-h -x])
