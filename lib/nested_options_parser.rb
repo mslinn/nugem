c69ca8babc53deda7610b6c029e9b94be336936c
Mike Slinn <mslinn@mslinn.com>
2025-06-25 21:56:17 -0400

    -

--- lib/nested_options_parser.rb
+++ lib/nested_options_parser.rb
@@ -1,5 +1,5 @@
 class NestedOptionParser
-  def initialize(option_parser, subcommand_parser = nil)
+  def initialize(option_parser_pro, subcommand_parser_proc = nil)
     @option_parser = option_parser
     @subcommand_parser = subcommand_parser
   end
