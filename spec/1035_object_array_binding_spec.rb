b0dbbdb4b9dc67744cfbd67198a37db6fc988629
Mike Slinn <mslinn@mslinn.com>
2025-08-31 12:45:28 -0400

    -

--- spec/1035_object_array_binding_spec.rb
+++ spec/1035_object_array_binding_spec.rb
@@ -8,55 +8,49 @@ RSpec.describe ObjectArrayBinding do
   let(:obj2) { Struct.new(:bar).new('bar from obj2') }
   let(:obj3) { Struct.new(:foo).new('foo from obj3') }
 
-  context 'when only one object responds' do
-    let(:oab) { described_class.new([obj1, obj2]) }
+  let(:oab12) { described_class.new([obj1, obj2]) }
+  let(:oab13) { described_class.new([obj1, obj3]) }
 
+  context 'when only one object responds' do
     it 'resolves foo from obj1' do
-      expect(oab.render('<%= foo %>')).to eq('foo from obj1')
+      expect(oab12.render('<%= foo %>')).to eq('foo from obj1')
     end
 
     it 'resolves bar from obj2' do
-      expect(oab.render('<%= bar %>')).to eq('bar from obj2')
+      expect(oab12.render('<%= bar %>')).to eq('bar from obj2')
     end
   end
 
   context 'when no object responds' do
-    let(:oab) { described_class.new([obj1, obj2]) }
-
     it 'raises NameError' do
-      expect { oab.render('<%= baz %>') }.to raise_error(NameError)
+      expect { oab12.render('<%= baz %>') }.to raise_error(NameError)
     end
   end
 
   context 'when multiple objects respond' do
-    let(:oab) { described_class.new([obj1, obj3]) }
-
     it 'raises NameError with ambiguity message' do
       expect do
-        oab.render('<%= foo %>')
+        oab13.render('<%= foo %>')
       end.to raise_error(AmbiguousMethodError, /Ambiguous method 'foo'/)
     end
   end
 
   context 'with respond_to?' do
-    let(:binding_providers12) { described_class.new([obj1, obj2]) }
-    let(:binding_providers13) { described_class.new([obj1, obj3]) }
-
     it 'returns true for foo' do
-      expect(binding_providers12.respond_to?(:foo)).to be true
+      expect(oab12.respond_to?(:foo)).to be true
     end
 
     it 'returns true for bar' do
-      expect(binding_providers12.respond_to?(:bar)).to be true
+      expect(oab12.respond_to?(:bar)).to be true
     end
 
     it 'returns false for baz' do
-      expect(binding_providers12.respond_to?(:baz)).to be false
+      expect(oab12.respond_to?(:baz)).to be false
     end
 
     it 'defines foo but raises AmbiguousMethodError if more than one object defines the desired method' do
-      expect(binding_providers13.respond_to?(:foo)).to be true
-      expect { binding_providers13.foo }.to raise_error(AmbiguousMethodError, /Ambiguous method 'foo'/)
+      expect(oab13.respond_to?(:foo)).to be true
+      expect { oab13.foo }.to raise_error(AmbiguousMethodError, /Ambiguous method 'foo'/)
     end
   end
 end
