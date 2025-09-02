585c6fe8feb9df8cd5d7fcc0489f20f7cc9c2f88
Mike Slinn <mslinn@mslinn.com>
2025-09-02 11:53:03 -0400

    -

--- spec/1150_directory_spec.rb
+++ spec/1150_directory_spec.rb
@@ -5,19 +5,19 @@ require_relative '../lib/nugem'
 
 module Nugem
   class TemplateTest
-    RSpec.describe 'Directory entry processing' do
+    RSpec.describe 'Directory entry processing'.cyan do
       def cleanup(dest_path_fq)
-        dirs = Dir.glob('*', base: dest_path_fq)
+        dirs = Dir.glob('.?*', base: dest_path_fq)
         if dirs.empty?
-          puts "Temporary directory #{dest_path_fq} is empty."
+          puts "  Temporary directory #{dest_path_fq} is empty.".yellow
         else
-          puts "Contents of #{dest_path_fq} were, before deletion:"
-          puts "  #{dirs.join("\n  ")}"
+          puts "  Contents of #{dest_path_fq} were, before deletion:".green
+          puts "    #{dirs.join("\n    ")}".rstrip.green
         end
         FileUtils.rm_rf dest_path_fq
       end
 
-      context 'when handling a regular file' do
+      context 'when handling a regular file'.cyan do
         dest_base = Dir.mktmpdir('nugem_spec_') # Cannot use let vars in after clause
         nugem = Nugem.new({
                             force:    true,
@@ -31,7 +31,7 @@ module Nugem
         source_path_fq = File.join File.expand_path('templates'), path_fragment
         dest_path_fq = "#{dest_base}/.rspec"
 
-        it 'just copies it' do
+        it 'just copies it'.cyan do
           nugem.directory_entry source_path_fq, dest_path_fq, path_fragment.end_with?('.tt')
           expect(File.exist?(dest_path_fq)).to be true
         end
@@ -39,7 +39,7 @@ module Nugem
         after { cleanup dest_base } # rubocop:disable RSpec/HooksBeforeExamples
       end
 
-      context 'when handling a template' do
+      context 'when handling a template'.cyan do
         dest_base = Dir.mktmpdir('nugem_spec_') # Cannot use let vars in after clause
         nugem = Nugem.new({
                             force:    true,
@@ -53,7 +53,7 @@ module Nugem
         source_path_fq = File.join File.expand_path('templates'), path_fragment
         dest_path_fq = "#{dest_base}/LICENCE.txt"
 
-        it 'copies to a file with a new name, set mode, and render contents' do
+        it 'copies to a file with a new name, set mode, and render contents'.cyan do
           nugem.directory_entry source_path_fq, dest_path_fq, path_fragment.end_with?('.tt')
           expect(File.exist?(dest_path_fq)).to be true
           # TODO: check mode
